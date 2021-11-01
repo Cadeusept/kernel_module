09e5ec67c9b7feb9ce72ffa28778f77d6f1ba672
Cadeusept <47189164+Cadeusept@users.noreply.github.com>
2021-11-02 00:23:56 +0300

    Add files via upload

--- chardev.c
+++ chardev.c
@@ -16,18 +16,10 @@ int init_module()
     }
 
     char_dev1=MKDEV(ret_val,0);
+    char_dev2=MKDEV(ret_val,1);
 
-    ret_val = register_chrdev(0, DEVICE2_NAME, &F2ops);
-
-    if (ret_val < 0) {
-        printk(KERN_ERR "Sorry, registering the character device failed with %d\n", ret_val);
-        return ret_val;
-    }
-
-    char_dev2=MKDEV(ret_val,0);
-
-    printk("Registration is a success. The major and minor %s numbers are %d %d.\n",DEVICE1_NAME, MAJOR(char_dev1), MINOR(char_dev1));
-    printk("Registration is a success. The major and minor %s numbers are %d %d.\n",DEVICE2_NAME, MAJOR(char_dev2), MINOR(char_dev2));
+    printk("Registration of device %s is a success. The major and minor numbers are %d %d.\n",DEVICE1_NAME, MAJOR(char_dev1), MINOR(char_dev1));
+    printk("Registration of device %s is a success. The major and minor numbers are %d %d.\n",DEVICE2_NAME, MAJOR(char_dev2), MINOR(char_dev2));
 
     return 0;
 }
